04e5ab983580a892ee99b93f8f63257320f69733
rishitaamehta <mehtr681@newschool.edu>
2022-04-03 23:20:01 -0400

    update

---
+++ Mad Libs/myscript.js
@@ -0,0 +1,66 @@
+console.log(document.getElementById("field01").value);
+
+document.getElementById("story").style.display = "none";
+document.getElementById("questions").style.display = "block";
+
+document.getElementById("myButton").onclick = function() {
+
+  document.getElementById("word01").innerHTML = document.getElementById("field01").value;
+
+
+  document.getElementById("word02").innerHTML = document.getElementById("field02").value;
+
+
+  document.getElementById("word03").innerHTML = document.getElementById("field03").value;
+
+
+  document.getElementById("word04").innerHTML = document.getElementById("field04").value;
+
+
+  document.getElementById("word05").innerHTML = document.getElementById("field05").value;
+
+
+  document.getElementById("word06").innerHTML = document.getElementById("field06").value;
+
+
+  document.getElementById("word07").innerHTML = document.getElementById("field07").value;
+
+
+
+  document.getElementById("word08").innerHTML = document.getElementById("field08").value;
+
+
+
+  document.getElementById("word09").innerHTML = document.getElementById("field09").value;
+
+
+
+  document.getElementById("word10").innerHTML = document.getElementById("field10").value;
+
+
+  document.getElementById("word11").innerHTML = document.getElementById("field11").value;
+
+
+  document.getElementById("word12").innerHTML = document.getElementById("field12").value;
+
+
+  document.getElementById("word13").innerHTML = document.getElementById("field13").value;
+
+
+  document.getElementById("word14").innerHTML = document.getElementById("field14").value;
+
+
+  document.getElementById("word15").innerHTML = document.getElementById("field15").value;
+
+
+  document.getElementById("word16").innerHTML = document.getElementById("field16").value;
+
+
+  document.getElementById("word17").innerHTML = document.getElementById("field17").value;
+
+
+  document.getElementById("story").style.display = "block";
+
+  document.getElementById("questions").style.display = "none";
+
+}
